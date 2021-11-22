1b364592e4486891425d6d446e0adf52b20f9615
sande_xx3phkx <sanderr06@gmail.com>
2021-11-22 17:11:08 +0100

    w

--- Project/servo_voor_test.ino
+++ Project/servo_voor_test.ino
@@ -27,6 +27,5 @@ void loop() {
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15 ms for the servo to reach the position
-    //hdawi hdsal;jkv dsk;heoshepuohwopuaudhjwopadhwaopudhwua
   }
 }
